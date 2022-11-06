0d824fe1758f36fbe0420b489ce453a52b38dbbc
blinikar <blinov.egor2013@mail.ru>
2022-11-06 16:56:39 +0300

    ready for test

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,19 +1,76 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import styles from './MainPage.module.scss';
 import { useMainPageLogic } from './MainPage.logic';
-import { CoolRectangle } from 'components/CoolRectangle/CoolRectangle';
+import { Option } from 'components/Option';
+import { useConfig } from 'hooks/useConfig';
 
 export interface MainPageProps {
 }
 
 export const MainPage: React.FC<MainPageProps> = (props) => {
   const logic = useMainPageLogic(props);
+  const config = useConfig();
+  const { values, getChangeValueHandler, getApplyActionHandler, getError, getValues } = logic.useForm(config);
 
-  return <div className={styles['centering-wrapper']}>
-    <div className={styles['content']}>
-      <h1>You are good to go!</h1>
-      <p> Check out this cool component below: </p>
-      <CoolRectangle/>
-    </div>
+  const submit = () => {
+    const xhr = new XMLHttpRequest();
+    xhr.open('POST', import.meta.env.VITE_API_BASE_URL + '/add_event', false);
+
+    xhr.setRequestHeader('Accept', 'application/json');
+    xhr.setRequestHeader('Content-Type', 'application/json');
+
+    xhr.send(JSON.stringify(getValues()));
+  };
+
+  useEffect(() => {
+    Telegram.WebApp.ready();
+    Telegram.WebApp.MainButton.setText('SAVE').show().onClick(function () {
+      submit();
+      Telegram.WebApp.close();
+    });
+  }, []);
+
+  return <div className={styles['content']}>
+    {values.length > 0 &&
+      values.map((option, index) => {
+        switch (option.type) {
+        case 'counter':
+          return <Option
+            key={index}
+            type={option.type}
+            label={option.label}
+            hint={option.hint}
+            currentValue={option.value}
+            error={getError(option.label)}
+            setValue={getChangeValueHandler(option.label)}
+            applyAction={getApplyActionHandler(option.label)}
+          />;
+        case 'text':
+          return <Option
+            key={index}
+            type={option.type}
+            label={option.label}
+            hint={option.hint}
+            currentValue={option.value}
+            error={getError(option.label)}
+            setValue={getChangeValueHandler(option.label)}
+          />;
+        case 'select':
+          return <Option
+            key={index}
+            type={option.type}
+            label={option.label}
+            hint={option.hint}
+            currentValue={option.value}
+            error={getError(option.label)}
+            setValue={getChangeValueHandler(option.label)}
+            options={option.options}
+          />;
+        }
+      })}
+
+    <button onClick={() => {
+      submit();
+    }}>TEST</button>
   </div>;
 };
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,3 +1,3 @@
 /// <reference types="vite/client" />
 // noinspection JSFileReferences
-declare const VITE_API_BASE_URL: string;
+declare const Telegram;
